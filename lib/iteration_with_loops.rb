771ecc07011fd6e0c7094fe57615c5076812330c
Roan Bascoe <roanbascoe5@gmail.com>
2020-05-27 15:15:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,9 @@
+require 'pry'
+
 def join_nested_strings(src)
     new_string = ""
     row_index = 0
+    binding.pry
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do

3e8d1f6a71fa25aea8eab4e6b547db5c0fd5fb83
Roan Bascoe <roanbascoe5@gmail.com>
2020-05-27 15:04:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,6 +7,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     string += src[row_index][element_index] + ''
+  end
     element_index += 1
   end
   row_index += 1
